38ed03a8b0e67ca4898fa29bbd8a4b6fa0d076fd
Tausif Fardin <tausiffardin4@gmail.com>
2024-01-09 13:08:54 +0600

    skill section add

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import HeaderSection from "@/components/header-section";
 import AboutMe from "@/components/about-me";
 import SectionTitle from "@/components/section-title";
+import Skills from "@/components/skills";
 export default function Home() {
   return (
     <div className="flex flex-col p-10">
       <HeaderSection />
       <SectionTitle title="About Me" />
       <AboutMe />
+      <SectionTitle title="Skills" />
+      <Skills />
     </div>
   );
 }
