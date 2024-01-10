1d1db34c4aabfa7b0ae7ae63c8ff6c312db9728c
Tausif Fardin <tausiffardin4@gmail.com>
2024-01-10 11:59:54 +0600

    project section add

---
+++ constants/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    githubLink: "http://therapystation.org/",
+    description: "Therapy Station",
+    thumbnail: "/P_1.png",
+  },
+  {
+    githubLink: "https://github.com/yourusername/project2",
+    description: "Project 2 Description",
+    thumbnail: "/project2_thumbnail.jpg",
+  },
+];
