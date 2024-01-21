876b76bd053c47ee5aba7f557d9d8edf9df4029f
Tausif Fardin <tausiffardin4@gmail.com>
2024-01-21 23:42:43 +0600

    link style update

--- src/components/publications.js
+++ src/components/publications.js
@@ -4,10 +4,13 @@ const Publications = () => {
   return (
     <div>
       <div className="font-bold text-xl">
-        Title : Stock Market Comparison and Analysis in Preceding and Following
-        Pandemic in Bangladesh using Machine Learning Approaches
+        Stock Market Comparison and Analysis in Preceding and Following Pandemic
+        in Bangladesh using Machine Learning Approaches
       </div>
-      <Link href={"https://dl.acm.org/doi/10.1145/3542954.3542993"}>
+      <Link
+        href={"https://dl.acm.org/doi/10.1145/3542954.3542993"}
+        className="text-blue-500 rounded-e-md p-2 bg-slate-200"
+      >
         Paper Link
       </Link>
     </div>
