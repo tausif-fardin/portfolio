acc7fd5d36a306b83956cc684d8a3e6230d7e166
Tausif Fardin <tausiffardin4@gmail.com>
2024-02-01 13:43:08 +0600

    experience section design update

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,36 +1,39 @@
+"use client";
+
 import React from "react";
+import { Briefcase } from "lucide-react";
 
 const Experience = ({ jobs }) => {
   return (
-    <main className="CV-page">
-      <div className="CV-grid">
-        <div className="CV-grid-column">
-          <div className="CV-jobs">
-            <h2 className="CV-secondaryHeading CV-jobs-heading">
-              Relevant Experience
-            </h2>
-            {jobs.map((job, index) => (
-              <section className="CV-timeline CV-job" key={index}>
-                <h3 className="CV-timeline-heading">
-                  <span className="CV-timeline-heading-title">{job.title}</span>
-                  &#8211;
-                  <span className="CV-timeline-heading-location">
-                    {job.location}
+    <main className="bg-gray-100 py-10 rounded-md">
+      <div className="container mx-auto">
+        <div className="relative pl-4">
+          {jobs.map((job, index) => (
+            <div key={index} className="mb-8 relative">
+              <div className="flex items-start mb-4">
+                <div className="border-2 border-[#354649] rounded-full h-8 w-8 flex items-center justify-center">
+                  <span className="text-white">
+                    <Briefcase color="#354649" size={18} />
                   </span>
-                  <small className="CV-timeline-heading-duration">
-                    {job.duration}
-                  </small>
-                </h3>
-                <ul className="CV-timeline-details">
-                  {job.details.map((detail, detailIndex) => (
-                    <li className="CV-job-timeline-item" key={detailIndex}>
-                      {detail}
-                    </li>
-                  ))}
-                </ul>
-              </section>
-            ))}
-          </div>
+                </div>
+                <div className="ml-4">
+                  <h3 className="text-xl font-bold text-gray-800">
+                    {job.title}
+                  </h3>
+                  <p className="text-gray-600">{job.location}</p>
+                  <p className="text-sm text-gray-600">{job.duration}</p>
+                </div>
+              </div>
+
+              <ul className="pl-12">
+                {job.details.map((detail, detailIndex) => (
+                  <li key={detailIndex} className="mb-2">
+                    - {detail}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </main>
